39436fa1dfbeffda3d4aae7542d8c812a95a61ec
Fofan <ivan.efimov3228@gmail.com>
2023-10-30 00:29:18 +0500

    Init

---
+++ SneakerCheck.Migrations/Factory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using SneakerCheck.Data;
+
+namespace SneakerCheck.Migrations;
+
+public class Factory : IDesignTimeDbContextFactory<SneakerCheckDbContext>
+{
+    public SneakerCheckDbContext CreateDbContext(string[] args)
+    {
+        var configurationBuilder = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .AddUserSecrets<Factory>();
+
+        var configuration = configurationBuilder.Build();
+        var optionsBuilder = new DbContextOptionsBuilder<SneakerCheckDbContext>();
+        SneakerCheckDbContext.Configure(optionsBuilder, configuration, typeof(Factory).Assembly);
+
+        return new SneakerCheckDbContext(optionsBuilder.Options);
+    }
+}
---
+++ SneakerCheck.Server/Controllers/Common/ApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SneakerCheck.Controllers.Common;
+
+[ApiController]
+public abstract class ApiController : ControllerBase
+{
+    
+}
